761e015d7755a1415957ba400cd4d17c4cee01cf
Sudarman <sud76643@gmail.com>
2025-01-31 09:44:45 +0700

    mengirim dan menerima data dari fragment category ke detail category fragment

---
+++ app/src/main/java/com/example/mynavcompt/CategoryFragment.kt
@@ -0,0 +1,44 @@
+package com.example.mynavcompt
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.mynavcompt.databinding.FragmentCategoryBinding
+
+class CategoryFragment : Fragment() {
+
+    private var _binding: FragmentCategoryBinding? = null
+    private  val  binding get() = _binding!!
+
+    companion object{
+        val EXTRA_NAME = "extra_name"
+        val EXTRA_DESCRIPTION = "extra_description"
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCategoryBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnDetailCategory.setOnClickListener { view ->
+            val mBundle = Bundle()
+            mBundle.putString(EXTRA_NAME, "Lifestyle")
+            mBundle.putLong(EXTRA_DESCRIPTION, 7)
+            view.findNavController().navigate(R.id.action_categoryFragment_to_detailCategoryFragment, mBundle)
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/mynavcompt/DetailCategoryFragment.kt
@@ -0,0 +1,39 @@
+package com.example.mynavcompt
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mynavcompt.databinding.FragmentCategoryBinding
+import com.example.mynavcompt.databinding.FragmentDetailCategoryBinding
+
+
+class DetailCategoryFragment : Fragment() {
+
+    private var _binding: FragmentDetailCategoryBinding? = null
+    private  val  binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailCategoryBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dataName = arguments?.getString(CategoryFragment.EXTRA_NAME)
+        val dataDescription = arguments?.getLong(CategoryFragment.EXTRA_DESCRIPTION)
+
+        binding.tvCategoryName.text = dataName
+        binding.tvCategoryDescription.text = "stok : $dataDescription"
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
